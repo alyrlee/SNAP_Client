66708da4ab07016a0c82c61a0378ba9bcc089fe9
Ashley Lee <ashleyloreal.lee@gmail.com>
2020-09-29 16:17:05 -0400

    update

--- src/Components/SnapLocationStores/SnapLocationsList.js
+++ src/Components/SnapLocationStores/SnapLocationsList.js
@@ -14,7 +14,7 @@ class SnapLocationsList extends Component {
       <React.Fragment>
         <div className="snaplocationslist">
           {this.props.snapLocationsList.map(SLL => { 
-            if (this.props.snapLocationList){this.props.snapLocationList.map(....)}
+            if (this.props.snapLocationList)(
             return (
               <div key={SLL._ObjectId} className="placename">
                 <h1 to={`/snaplocations/${SLL._ObjectId}`}>

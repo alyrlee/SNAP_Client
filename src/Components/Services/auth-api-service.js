ce6d652eff237506e111526715b25fa8e7878a30
Ashley Lee <ashleyloreal.lee@gmail.com>
2020-10-27 12:29:56 -0400

    fixed syntax errors, removed unecesssary code

--- src/Components/Services/auth-api-service.js
+++ src/Components/Services/auth-api-service.js
@@ -1,28 +1,16 @@
 import config from '../../config';
 
-const userName = 'DemoUser2020'
-const password = 'DemoUser*'
-
-fetch(`${config.API_ENDPOINT}/auth/login`, {
-    method: 'POST',
-    headers: {
-        Authorization: `Schema ${userName}:${password}`    
-    },
-    body: JSON.stringify({
-        userName, 
-        password
-    })
-})
+const testUserName = 'DemoUser2020';
+const testPassword = 'DemoUser*';
 
 const AuthApiService = {
-    // client-side POST for handling user logins. //
-    postLogin({user_name, password}) {
+    postLogin({userName, password}) {
         return fetch(`${config.API_ENDPOINT}/auth/login`, {
             method: 'POST',
             headers: {
                 'content-type': 'application/json',
             },
-            body: JSON.stringify({user_name, password})
+            body: JSON.stringify({userName, password})
         })
             .then(res => {
                 return (!res.ok)
@@ -30,49 +18,46 @@ const AuthApiService = {
                     : res.json()
             });
     },
-    // client-side POST for registering new users. //
+
     postUser(user) {
         return fetch(`${config.API_ENDPOINT}/users`, {
             method: 'POST',
             headers: {
                 'content-type': 'application/json',
             },
-            body: JSON.stringify(user)
+            body: JSON.stringify(user),
         })
-            .then(res => {
-                return (!res.ok)
-                    ? res.json().then(e => Promise.reject(e))
-                    : res.json()
-            });
+        .then(res => {
+            return (!res.ok)
+                ? res.json().then(e => Promise.reject(e))
+                : res.json()
+        });
+    },  
+
+    getStores(stores,userName, password) {
+        return fetch(`${config.API_ENDPOINT}/stores`, {
+            method: 'GET',
+            headers: {
+                Authorization: `Schema ${userName}:${password}`,
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify(stores),
+            // ObjectId: 1,
+            // Store_Name: "Walmart Super Center"
+        })
+        .then(res => {
+            return (!res.ok)
+            ? res.json().then(e => Promise.reject(e))
+            : res.json()
+        });
     },
 
-
-getStores(stores,userName, password) {
-    return fetch(`${config.API_ENDPOINT}/stores`, {
-        method: 'GET',
-        headers: {
-        Authorization: `Schema ${userName}:${password}`,
-        'content-type': 'application/json',
-        },
-        body: JSON.stringify(stores)
-        // ObjectId: 1,
-        // Store_Name: "Walmart Super Center"
-    })
-    .then(res => {
-        return (!res.ok)
-        ? res.json().then(e => Promise.reject(e))
-        : res.json()
-    });
-},
-
-
-//client side POST for store locations when user us logged in //
     postStores(stores,userName, password) {
         return fetch(`${config.API_ENDPOINT}/stores`, {
             method: 'POST',
             headers: {
-            Authorization: `Schema ${userName}:${password}`,
-            'content-type': 'application/json',
+                Authorization: `Schema ${userName}:${password}`,
+                'content-type': 'application/json',
             },
             body: JSON.stringify(stores)
             // ObjectId: 1,
@@ -83,8 +68,47 @@ getStores(stores,userName, password) {
             ? res.json().then(e => Promise.reject(e))
             : res.json()
         });
+    },
+
+    getSavedLocations(ObjectId, Store_Name, userName, password) {
+        return fetch(`${config.API_ENDPOINT}/SavedLocations`, {
+            method: 'GET',
+            headers: {
+                Authorization: `Schema ${userName}:${password}`,
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify({
+                ObjectId: ObjectId,
+                Store_Name: Store_Name
+            })
+        })
+        .then(res => {
+            return (!res.ok)
+            ? res.json().then(e => Promise.reject(e))
+            : res.json()
+        });
+    },
+    
+    postSavedLocations(ObjectId, Store_Name, userName, password) {
+        return fetch(`${config.API_ENDPOINT}/SavedLocations`, {
+            method: 'POST',
+            headers: {
+                Authorization: `Schema ${userName}:${password}`,
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify({
+                ObjectId: ObjectId,
+                Store_Name: Store_Name,
+            })
+        })
+        .then(res => {
+            return (!res.ok)
+            ? res.json().then(e => Promise.reject(e))
+            : res.json()  
+        });
     }
-}
+}   
+
+
 
 export default AuthApiService;
-

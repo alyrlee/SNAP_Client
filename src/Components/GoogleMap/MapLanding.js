8100fe29214c105fb4e86a8768e6896fe9fa0ea3
Ashley Lee <ashleyloreal.lee@gmail.com>
2021-01-04 08:02:40 -0500

    removed commented out code

--- src/Components/GoogleMap/MapLanding.js
+++ src/Components/GoogleMap/MapLanding.js
@@ -1,8 +1,6 @@
 import React, {Component} from 'react';
 import config from '../../config';
-// import data from  './SnapData';
 import data from './SnapDataShort';
-import SnapLocationsList from '../SnapLocationStores/SnapLocationsList';
 import MapContainer from '../GoogleMap/GooglePlaces';
 
 export default class MapLanding extends Component {
@@ -38,31 +36,4 @@ export default class MapLanding extends Component {
     }
   }
 
-    //manipulate based upon how data is returned
-    //ex: 
-    // const data =[
-      //     {
-      //       // x   |     y      | objectid |                     store_name                     |                address                | address_line__2 |  city  | state | zip5 | zip4 | county  |  longitude  |  l
-      //       // -71.0622020 | 42.3649290 |     4030 | 7-eleven 32476C                                    | 91 Causeway St                        | 91-99           | Boston | MA    | 2114 | 1308 | SUFFOLK | -71.0622020 | 42.3649290 | 2020-12-15 02:22:00-05
-      //       X: -71.0622020,
-      //       Y: 42.36492920,
-      //       objectid: 4030,
-      //       store_name: '7-eleven 32476C',
-      //       address: '91 Causeway St',
-      //       city: 'Boston',
-      //       state: 'MA'  
-      //     },
-      //     {
-      //       // x   |     y      | objectid |                     store_name                     |                address                | address_line__2 |  city  | state | zip5 | zip4 | county  |  longitude  |  l
-      //       // -71.0622020 | 42.3649290 |     4030 | 7-eleven 32476C                                    | 91 Causeway St                        | 91-99           | Boston | MA    | 2114 | 1308 | SUFFOLK | -71.0622020 | 42.3649290 | 2020-12-15 02:22:00-05
-      //       X: -71.0622021,
-      //       Y: 42.36492922,
-      //       objectid: 4031,
-      //       store_name: '8-eleven 32476C',
-      //       address: '93 Causeway St',
-      //       city: 'Boston',
-      //       state: 'MA'  
-      //     }
-      //   ]
-
-
+   

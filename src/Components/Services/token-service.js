1be75d52db9f8452ec18d0cb2540fb72ee035d52
Ashley Lee <ashleyloreal.lee@gmail.com>
2021-02-01 10:37:51 -0500

    save to local storag

--- src/Components/Services/token-service.js
+++ src/Components/Services/token-service.js
@@ -3,7 +3,7 @@ import config from '../../config';
 
 const TokenService = {
     saveAuthToken(token) {
-        window.sessionStorage.setItem(config.TOKEN_KEY, token);
+        window.localStorage.setItem(config.TOKEN_KEY, token);
     },
     saveUserName(user_name){
         window.localStorage.setItem(config.USER_NAME, user_name)
@@ -15,10 +15,10 @@ const TokenService = {
         return window.localStorage.getItem(config.USER_NAME)
       },
     getAuthToken() {
-        return window.sessionStorage.getItem(config.TOKEN_KEY);
+        return window.localStorage.getItem(config.TOKEN_KEY);
     },
     clearAuthToken() {
-        window.sessionStorage.removeItem(config.TOKEN_KEY);
+        window.localStorage.removeItem(config.TOKEN_KEY);
     }, 
     hasAuthToken() {
         return !!TokenService.getAuthToken();

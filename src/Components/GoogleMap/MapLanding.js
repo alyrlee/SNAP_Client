bf55407baf58d2b8814f87ecabc6f1851305760c
Ashley Lee <ashleyloreal.lee@gmail.com>
2021-02-18 11:06:24 -0500

    defined JSON

--- src/Components/GoogleMap/MapLanding.js
+++ src/Components/GoogleMap/MapLanding.js
@@ -6,7 +6,7 @@ import snapLocationsList from '../SnapLocationStores/SnapLocationsList';
 export default class MapLanding extends Component {
   constructor(props){
    super(props);
-   this.state = {stores: [],error: null } ;
+   this.state = {data: [], stores: [],error: null } ;
   }
 
   // onSelect = () => {
@@ -21,31 +21,30 @@ export default class MapLanding extends Component {
   //   }
 
 componentDidMount(){
+  const resJSON=
   AuthApiService.getStores()
   .then(resJSON => {
-    this.setState({ stores: resJSON});
-          console.log('stores json data', resJSON);
-        })
-}
+    console.log('data', resJSON());
+        console.log('data 2',resJSON.data.results);
+      });
+    this.setState({ snapLocationsList: resJSON.data.results,
+      data: resJSON});
+      console.log('json data', resJSON);
+    }
+
 // {this.markers = resJSON && !error ? resJSON.slice(0, 200): []}
 
    render() {
-      return (
-        <div className="ui container" style={{ marginTop: '10px' }}>
-          {/* <snapLocationsList stores={this.state.resJSON} /> 
-          Found: {this.state.resJSON.length} SnapLocationsList */}
-        
-          <MapContainer 
-            stores={this.state.resJSON} 
-            data={this.state.resJSON}
-            // onClick={this.onMarkerClustererClick} 
-            markers = {this.state.resJSON}  
-         />  
-         <snapLocationsList />
-        </div>
-        
-         
+    return (
+      <div className="ui container" style={{ marginTop: '10px' }}>
+        {/* <SnapLocationsList stores={this.state.data} />
+        Found: {this.state.data.length} SnapLocationsList */}
+        <MapContainer 
+          snapLocationsList={this.state.data} 
+          // onSelect={this.onSelect} 
+          markers = {this.state.data}  
+       />  
+      </div>
     );
   }
 }
-

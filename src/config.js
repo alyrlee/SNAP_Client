fe57b801dbe46d3637acf3e6460a98f73e203986
Ashley Lee <ashleyloreal.lee@gmail.com>
2021-03-22 09:54:52 -0400

    prepare to deploy

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    API_ENDPOINT: "http://localhost:8000/api",
-    // API_ENDPOINT: "https://murmuring-shore-59851.herokuapp.com/",
+    // API_ENDPOINT: "http://localhost:8000/api",
+    API_ENDPOINT: "https://murmuring-shore-59851.herokuapp.com/",
     TOKEN_KEY: 'snap-client-auth-login'
 }

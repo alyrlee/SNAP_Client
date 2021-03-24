18b12ba9d09fbf48e1ec0e13c3b9a9dc287ecc6f
Ashley Lee <ashleyloreal.lee@gmail.com>
2021-03-24 11:49:51 -0400

    prepare to deploy

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ export default {
     // API_ENDPOINT: "http://localhost:8000/api",
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    API_ENDPOINT: process.env.API_ENDPOINT || "https://murmuring-shore-59851.herokuapp.com/",
+    API_ENDPOINT: process.env.API_ENDPOINT || "https://murmuring-shore-59851.herokuapp.com/api",
     TOKEN_KEY: process.env.TOKEN_KEY || 'snap-client-auth-login'
 }
